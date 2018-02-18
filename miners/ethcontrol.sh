fb3068bf0dfdee55699c12217f0857aa1179ba18
farm4 <kardapolov@gmail.com>
2018-02-18 15:28:26 +0500

    ETH Control for nvidia

---
+++ miners/ethcontrol.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Reading config file
+. ~/miners/config.cfg
+
+cardsStr=$(nvidia-smi | grep -B1 "Default" | grep -oE "|[[:space:]]*[0-9]*[[:space:]]*GeForce" | grep -oE "[0-9]*")
+gpus=(${cardsStr})
+
+#Setting powerlimits
+
+echo "123321" | sudo -S nvidia-smi -pl $powerLimit
+
+#Overclocking
+for i in ${gpus[@]}
+do
+	nvidia-settings -a [gpu:$i]/GPUPowerMizerMode=1
+	nvidia-settings -a [gpu:$i]/GPUGraphicsClockOffset[3]=${equihashCore[$i]}
+	nvidia-settings -a [gpu:$i]/GPUMemoryTransferRateOffset[3]=${equihashMemory[$i]}
+done
+
+#Watchdog
+gnome-terminal -e "./miners/watchdog.sh"
+
+#Start zencash mining over zenmine.pro and bittrex
+~/ethcontrol/EthControl -accessToken $accessToken -rigName $rigName

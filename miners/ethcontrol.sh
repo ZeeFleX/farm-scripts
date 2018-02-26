36933811d6f3a5375d91496d442e5cbc77f484ed
farm4 <kardapolov@gmail.com>
2018-02-26 17:35:31 +0500

    Fix reading config file

--- miners/ethcontrol.sh
+++ miners/ethcontrol.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #Reading config file
-. ~/miners/config.cfg
+. /home/farm4/miners/config.cfg
 
-#Start zencash mining over zenmine.pro and bittrex
-~/ethcontrol/EthControl -accessToken $accessToken -rigName $rigName
+#Start ethereum mining
+echo $rigName
+echo $accessToken
+/home/farm4/ethcontrol/EthControl -accessToken $accessToken -rigName $rigName

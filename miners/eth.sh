0e480562a47ccf13eb90d48221040fe4b8769b9f
farmer <kardapolov@gmail.com>
2018-05-28 22:31:16 +0500

    ETH ethermine pool in config file and ZCash script

--- miners/eth.sh
+++ miners/eth.sh
@@ -12,5 +12,15 @@ export GPU_USE_SYNC_OBJECTS=1
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_SINGLE_ALLOC_PERCENT=100
 
-~/miners/claymore/ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal $ethAddress/$rigName/$email -mode 1 -wd 0
+#Start ETH mining
+case "$ethPool" in
+    "0")
+    ~/miners/claymore/ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal $ethAddress/$rigName/$email -mode 1 -wd 0
+    ;;
+    "1")
+    ~/miners/claymore/ethdcrminer64 -epool eu1.ethpool.org:3333 -ewal $ethAddress.$rigName -mode 1 -wd 0
+    ;;
+esac
+
+
 
---
+++ miners/zec.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ln -s ~/.local/share/applications/Ethereum.desktop ~/.config/autostart/startminer.desktop -f
+
+#Reading config file
+. ~/miners/config.cfg
+
+#Start ZEC mining
+case "$zecPool" in
+    "0")
+    ~/miners/claymore_zcash/zecminer64 -zpool ssl://zec-eu1.nanopool.org:6633 -zwal $zecAddress.$rigName/$email -zpsw z -wd 0 -ftime 1
+    ;;
+esac
+
+
+
